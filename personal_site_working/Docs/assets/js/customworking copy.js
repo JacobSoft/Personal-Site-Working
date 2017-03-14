88423a190f3e547afe39501c4b6ff62e72d50452
RobertRedford <clarksjake@gmail.com>
2017-03-14 16:22:48 -0700

    All files
    Added all files

---
+++ personal_site_working/Docs/assets/js/customworking copy.js
@@ -0,0 +1,37 @@
+!function ($) {
+
+	$(function () {
+
+		window.prettyPrint && prettyPrint()
+
+		/* ---------------------------------------------- /*
+		 * Animation scroll
+		/* ---------------------------------------------- */
+		
+		$('a[href*=#]').bind('click', function(e) {
+			var anchor = $(this);
+
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 500);
+			e.preventDefault();
+		});
+		
+		 var $body   = $(document.body)
+			
+			    $body.scrollspy({
+			      target: '.sidebar'
+			    });
+
+
+
+		
+	})
+
+	$(function() {
+	$('[data-toggle="popover"]').popover();
+});
+
+}(jQuery)
+				
+
